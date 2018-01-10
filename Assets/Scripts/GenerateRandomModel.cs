0fcc5026fdb7f4e2db06b60719b68b3c3cc60780
Alexander Watson <msnalexwatson@hotmail.co.uk>
2018-01-10 04:39:15 +0000

    Created script that randomly selects models
    Created script that selects random models and applies them to a slot and piece.
    
    Currently not rendering properly.

---
+++ Assets/Scripts/GenerateRandomModel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateRandomModel : MonoBehaviour 
+{
+	public GameObject piece;
+	public Mesh[] models;
+	public Material[] materials;
+	public Material background;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		MeshFilter slotMeshFilter = GetComponent<MeshFilter>();
+		int index = (int)Random.Range(0.0f, (float)models.Length);
+		slotMeshFilter.mesh = models[index];
+		string tag = slotMeshFilter.mesh.name;
+		tag = tag.Remove (0, tag.LastIndexOf ('_') + 1);
+		tag = tag.Remove (tag.LastIndexOf(' '));
+		gameObject.tag = tag;
+		gameObject.GetComponent<MeshRenderer>().material = new Material(background);
+
+		piece.AddComponent<MeshFilter>();
+		piece.AddComponent<MeshRenderer>();
+		MeshFilter pieceMeshFilter = piece.GetComponent<MeshFilter>();
+		pieceMeshFilter.mesh = slotMeshFilter.mesh;
+		piece.tag = tag;
+		piece.GetComponent<MeshRenderer>().material = new Material(materials[0]);
+	}
+}
