3cde16be2f683ceb48ce5c3dc0b085450dd4f38a
Alexander Watson <msnalexwatson@hotmail.co.uk>
2018-01-14 17:38:27 +0000

    Pieces cannot be dragged once placed.
    NOTE: If each slot was guaranteed to have a different shape from another slot it would prevent placing an incorrect piece in the slot.

--- Assets/Scripts/ChildLayout.cs
+++ Assets/Scripts/ChildLayout.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class ChildLayout : MonoBehaviour
 {
 	private Transform child;
-	private Transform grandChild;
 	private int startChildCount;
 	private int previousChildCount;
 
@@ -18,8 +17,6 @@ public class ChildLayout : MonoBehaviour
 	// When new children are added added their local position is centered.
 	void Update () 
 	{
-		Debug.Log(name);
-		Debug.Log(transform.childCount);
 		if (transform.childCount <= startChildCount)
 			return;
 
@@ -28,13 +25,8 @@ public class ChildLayout : MonoBehaviour
 
 		previousChildCount = transform.childCount;
 		child = transform.GetChild(transform.childCount - 1);
-		grandChild = child.GetChild (child.childCount - 1);
-
+		
 		if (Drag.ItemBeingDragged == null)
-		{
-			child.localPosition = new Vector3 ();
-			if (grandChild != null)
-				grandChild.localPosition = new Vector3 ();
-		} 
+			child.localPosition = new Vector3(); 
 	}
 }
