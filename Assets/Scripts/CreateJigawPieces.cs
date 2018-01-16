4c3d817522d45c916725877b32a7289293c56402
Alexander Watson <msnalexwatson@hotmail.co.uk>
2018-01-16 14:38:37 +0000

    MainGame Randomised Texture

--- Assets/Scripts/CreateJigawPieces.cs
+++ Assets/Scripts/CreateJigawPieces.cs
@@ -4,20 +4,24 @@ using System.Collections;
 
 public class CreateJigawPieces : MonoBehaviour
 {
-	public Texture2D Cartoon;
+	public Texture2D[] Textures;
 	public GameObject[] Slots;
+
+	private Texture2D cartoon;
 	private int columns;
 	private int rows;
 	private int height;
 	private int width;
 	
-	void Start ()
+	void Awake ()
 	{
 		// Defines the size of each jigsaw piece.
 		rows = 3;
 		columns = 3;
-		height = Cartoon.height / rows;
-		width = Cartoon.width / columns;
+		cartoon = Textures [Random.Range (0, Textures.Length)]; // Grabs Random texture.
+
+		height = cartoon.height / rows;
+		width = cartoon.width / columns;
 
 		int count = 0;
 		for (int i = 0; i < columns; i++)
@@ -33,7 +37,7 @@ public class CreateJigawPieces : MonoBehaviour
 
 	GameObject CreatePiece(float xSpriteIndex, float ySpriteIndex)
 	{
-		Sprite newSprite = Sprite.Create(Cartoon, new Rect(xSpriteIndex, ySpriteIndex, width, height), new Vector2());
+		Sprite newSprite = Sprite.Create(cartoon, new Rect(xSpriteIndex, ySpriteIndex, width, height), new Vector2());
 		
 		GameObject piece = new GameObject();
 		Image newImage = piece.AddComponent<Image>();
